5d8618a701d6c3cca12cba5d773881e5b509de71
patrickqcf <45515053+patrickqcf@users.noreply.github.com>
2020-07-11 22:13:03 -0300

    Inicio Contato

--- js/will.js
+++ js/will.js
@@ -12,24 +12,45 @@ $( document ).ready(function() {
         event.preventDefault();
         init();
     })
+    $('.contatolink').click(function(event){
+        event.preventDefault();
+        initContato();
+    })
+    $('.whatsappEnviar').click(function(event){
+        event.preventDefault();
+        var nome = $('#nome').val();
+        var mensagem = $('#mensagem').val();
+        var url = 'https://api.whatsapp.com/send?phone=5518991300284&text=Nome:%20' + nome + '%20Mensagem:%20' + mensagem;
+        window.location.href = url;
+    })
 
 });
 
 function init(){
     $(window).scrollTop(0);
+    $('.contato').hide();
     $('.home').show();
     $('.food-gallery').hide();
     $('.people-gallery').hide();
 }
 function initFoodGallery(){
     $(window).scrollTop(0);
+    $('.contato').hide();
     $('.home').hide();
     $('.food-gallery').show();
     $('.people-gallery').hide();
 }
 function initPeopleGallery(){
     $(window).scrollTop(0);
+    $('.contato').hide();
     $('.home').hide();
     $('.food-gallery').hide();
     $('.people-gallery').show();
+}
+function initContato(){
+    $(window).scrollTop(0);
+    $('.contato').show();
+    $('.home').hide();
+    $('.food-gallery').hide();
+    $('.people-gallery').hide();
 }
