535de0dc1f87a0e13e8c663d31ae0d092ce9b833
patrickqcf <45515053+patrickqcf@users.noreply.github.com>
2020-07-15 17:01:54 -0300

    Tentativa de capturar o evento back 2

--- js/will.js
+++ js/will.js
@@ -1,9 +1,6 @@
-$(document).on("pagecontainerbeforechange", function (e, data) {
-    if (typeof data.toPage == "string" && data.options.direction == "back") {
-        data.toPage = "patrickqcf.github.io";
-        data.options.transition = "flip";
-    }
-});
+window.addEventListener("hashchange", function(e) {
+    location.reload();
+  })
 $( document ).ready(function() {
     init();
     $('.strongWhatsapp').click(function(){
